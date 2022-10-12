e519e9b3a376bc94973dd791f4691fe870881d3b
Militto <milad.purahmad78@gmail.com>
2022-10-13 01:27:45 +0330

    [ADD] : Add docs to facade.

--- src/Facades/MilitoResponseGenerator.php
+++ src/Facades/MilitoResponseGenerator.php
@@ -2,7 +2,15 @@
 namespace Milito\ResponseGenerator\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Milito\ResponseGenerator\States\Failed\FailedState;
+use Milito\ResponseGenerator\States\Success\SuccessState;
 
+/**
+ * @method static SuccessState success()
+ * @method static FailedState failed()
+ *
+ * @see \Milito\ResponseGenerator\MilitoResponseGenerator
+ */
 class MilitoResponseGenerator extends Facade
 {
     protected static function getFacadeAccessor(): string
